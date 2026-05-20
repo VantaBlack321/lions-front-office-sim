391ea582fffcd09d55b9e60718e38649fb7748ff
VantaBlack321 <76549719+VantaBlack321@users.noreply.github.com>
2026-05-20 11:03:56 -0400

    Lions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+console.log("Hello, welcome to the Lions GM Simulator!");
+
+const playerRoster = [];
+
+function insertPlayer(Name, Position, Age, Overall) {
+    console.log("Name: " + Name + ", Position: " + Position + ", Age: " + Age + ", Overall: " + Overall);
+    playerRoster.push({
+        Name: Name,
+        Position: Position,
+        Age: Age,
+        Overall: Overall
+    });
+    // Here you would add code to actually insert the player into your data structure or database
+}
+
+insertPlayer("Jared Goff", "QB", "31", "80");
