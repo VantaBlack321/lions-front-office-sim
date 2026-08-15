6d2be0e97159b5650f9b842c343a29564ba02bb3
VantaBlack321 <76549719+VantaBlack321@users.noreply.github.com>
2026-08-15 19:10:11 -0400

    Modify

--- script_tutorial.js
+++ script_tutorial.js
@@ -6,5 +6,15 @@ const removedRoster = []
 
 const tradedRoster = []
 
+function addRoster(name, position, age, overall, salaryCap) {
+    playerRoster.append(name) = name
+    playerRoster.append(position) = position
+    playerRoster.append(age) = age
+    playerRoster.append(overall) = overall
+    playerRoster.append(salaryCap) = salaryCap
+}
+
+addRoster(10, 10, 10, 99, 9000)
+
 
 
