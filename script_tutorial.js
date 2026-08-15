a18bc23f2e3e77ee8ea710ccd23990c8b2817e0c
VantaBlack321 <76549719+VantaBlack321@users.noreply.github.com>
2026-08-15 19:10:56 -0400

    Modified Again

--- script_tutorial.js
+++ script_tutorial.js
@@ -2,19 +2,6 @@ console.log("This is the tutorial Lions Simulator")
 
 const playerRoster = []
 
-const removedRoster = []
-
-const tradedRoster = []
-
-function addRoster(name, position, age, overall, salaryCap) {
-    playerRoster.append(name) = name
-    playerRoster.append(position) = position
-    playerRoster.append(age) = age
-    playerRoster.append(overall) = overall
-    playerRoster.append(salaryCap) = salaryCap
-}
-
-addRoster(10, 10, 10, 99, 9000)
 
 
 
